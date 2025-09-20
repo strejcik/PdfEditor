da0d11ad877c135b23436197deba21e119c4bf61
strejcik <ontrox@hotmail.com>
2025-09-20 12:55:40 +0200

    Changed path of build folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (_, res) => res.sendFile(path.join(__dirname, "dist", "index.html")));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (_, res) => res.sendFile(path.join(__dirname, "build", "index.html")));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("listening on " + PORT));
